0cd21708df67617f5c0989eb37c13306fd283610
shruti <shrutimoradiya1@gmail.com>
2024-03-21 19:02:44 +0530

    add commit

--- src/App.js
+++ src/App.js
@@ -6,42 +6,50 @@ function App() {
      <>
       <div className='container mt-10'>
         <div className="App App-header">
-          <h3><span>Welcome</span> Manushri </h3>
-          <p>Loyalty Program Name</p>
+        <div class="row">
+          <div className='col-lg-11'>
+          <h3><span>Welcome</span> Manushri</h3> 
+          </div>
+          <div className='col-lg-1'>
+            <h3><span>x</span></h3> 
+          </div>
+         </div>
+         <p>Loyalty Program Name</p>
           <h2>Tier 1 Member</h2>
           <p className='tier'>Member since Octomber, 2023</p>
           
-          <div class="container text-center">
-            <div class="row">
-              <div class="col">2024
-                <div class="row">
-                <div class="col">Zen Coins</div>
+          <div className="container text-center fns">
+            <div className="row">
+              <div className="col"><button type="button" class="btn-secondary" disabled>2024</button>
+                <div className="row">
+                <div className="col cont">Zen Coins</div>
                 </div>
               </div>
               
-              <div class="col">20
-                <div class="row">
-                  <div class="col">Available Rewards</div>
+              <div className="col">20
+                <div className="row">
+                  <div className="col cont">Available Rewards</div>
                 </div>
               </div>
-              <div class="col">$234s
-                <div class="row">
-                  <div class="col">Your Savings</div>
+              <div className="col">$234s
+                <div className="row">
+                  <div className="col cont">Your Savings</div>
                 </div>
               </div>
             </div>
         </div>
 
-        <div class="container">
-          <input type="range" class="form-range" id="customRange1"></input>
-          <label for="customRange1" class="form-label">Tier 1 </label> 
-          <label for="customRange1" class="form-label">Tier 2 <span className='tier'>10000</span></label>
-          <label for="customRange1" class="form-label">Tier 3 <span className='tier'>10000</span></label>
+        <div className="container">
+          <input type="range" className="form-range" id="customRange1"></input>
+          <label for="customRange1" className="form-label">Tier 1 </label> 
+          <label for="customRange1" className="form-label">Tier 2 <span className='tier'>10000</span></label>
+          <label for="customRange1" className="form-label">Tier 3 <span className='tier'>10000</span></label>
         </div>
 
         </div>
       </div>
       
+
     </>
   );
 }
